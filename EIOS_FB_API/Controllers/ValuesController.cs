0a07f4de466efdd8b249ca2c71a7a5283b453934
Lu Lian Chyn <CMG.LINE.MESSAGE@gmail.com>
2019-07-08 16:05:33 +0800

    Test

--- EIOS_FB_API/Controllers/ValuesController.cs
+++ EIOS_FB_API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace EIOS_FB_API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Test" };
         }
 
         // GET api/values/5
